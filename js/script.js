15f1fb910719a8c387d2ab185e70addcd4ee425e
Kimberly <kimolanga123@gmail.com>
2022-03-19 21:37:01 +0300

    add script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,34 @@
 $(document).ready(function(){
-    $('.toppings').selectpicker();
+    //show hide popover
+    $(".dropdown").click(function(){
+        $(this).find(".dropdown-menu").slideToggle("fast")
+    });
+    $(document).on("click",function(event){
+        var $trigger =
+        $(".dropdown");
+        if($trigger !== 
+            event.target && 
+            !$trigger.has(event.target).length) {
+                $(".dropdown-menu").slideup("fast");
+            }
+    })
 })
+var total = 0;
+$("#items").on('click',".item", function(){
+    $(this).appendTo("#basket")
+    getTotal
+});
+$("#basket").on('click', ".item", function(){
+    $(this).appendTo("#items");
+    getTotal()
+});
+function getTotal(){
+    total = 0;
+    $("#basket").find('.price').each(function(i){
+        total += parseInt($(this).text().slice(0,-1));
+        if(i + 1 === $("#basket").find('.item').length){
+            $('.total_price').text(total+'$');
+        }
+    })
+}
+   
